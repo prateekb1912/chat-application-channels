d31cc830c38dd3a7102fc7ce99c56905880aa876
prateekb1912 <prateekb1912@gmail.com>
2023-01-09 19:31:42 +0530

    Send and receive messages single-user

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,5 +7,15 @@ class ChatConsumer(WebsocketConsumer):
 
         self.send(text_data=json.dumps({
             'type': 'message',
-            'data': 'Connected!'
+            'message': 'Connected!'
+        }))
+    
+    def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+
+        message = text_data_json['message']
+
+        self.send(text_data=json.dumps({
+           'type': 'message',
+           'message': message 
         }))
--- chat/static/js/script.js
+++ chat/static/js/script.js
@@ -1,4 +1,6 @@
 let url = `ws://${window.location.host}/ws/socket-server/`
+let form = document.getElementById('chat-form')
+const main = document.getElementById('main')
 
 const chatSocket = new WebSocket(url)
 
@@ -6,7 +8,30 @@ chatSocket.onopen = () => {
     console.log("Websocket connection established");
 }
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let msg = e.target.message.value
+    console.log(msg)
+    chatSocket.send(JSON.stringify({
+        'message': msg
+    }))
+
+    form.reset()
+})
+
 chatSocket.onmessage = (e) => {
     let data = JSON.parse(e.data);
     console.log(data)
+    
+    addMessage(data['message'])
+}
+
+function addMessage(data) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+
+    p.innerText = data;
+    div.appendChild(p);
+
+    main.appendChild(div);
 }
