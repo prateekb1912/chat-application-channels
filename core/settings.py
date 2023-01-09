f7be49c1d39b53cd387e5bbfa11c0f6b3c7f7585
prateekb1912 <prateekb1912@gmail.com>
2023-01-09 19:44:38 +0530

    Add channel layers for broadcasting messages

--- core/settings.py
+++ core/settings.py
@@ -75,15 +75,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
 
-# # Channel Layers
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             'hosts': [('127.0.0.1', 6379)],
-#         }
-#     }
-# }
+# Channel Layers
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    }
+}
 
 
 # Database
