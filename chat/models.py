4ad0177f06f434d2e166ca382212ae7a4a846e79
prateekb1912 <prateekb1912@gmail.com>
2023-02-01 15:17:07 +0530

    Add message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    content = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'[{self.type}] {self.user.username}: {self.content}'
+
